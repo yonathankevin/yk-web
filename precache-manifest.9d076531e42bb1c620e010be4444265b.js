37d82637cc4346f147ee68a33dc30dbf58dedbe9
Yonathan Kevin <kevinalmando99@gmail.com>
2022-09-22 20:46:26 +0700

    Updates

---
+++ precache-manifest.9d076531e42bb1c620e010be4444265b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35421ceb34f3b1af781",
+    "url": "/yk-web/static/css/main.df535817.chunk.css"
+  },
+  {
+    "revision": "a35421ceb34f3b1af781",
+    "url": "/yk-web/static/js/main.a35421ce.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/yk-web/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/yk-web/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "6fa4fcca62f5235ea7a3",
+    "url": "/yk-web/static/js/runtime~main.6fa4fcca.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/yk-web/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "94e9d7ff18bd70b4fde28ca41618f890",
+    "url": "/yk-web/index.html"
+  }
+];
